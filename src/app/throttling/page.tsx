06139def0781d53fd7fa24e552bc6e3dd73ac890
Luciano Ribeiro <luciano.ar6@gmail.com>
2024-04-06 05:05:26 -0300

    add throttling page

---
+++ src/app/throttling/page.tsx
@@ -0,0 +1,67 @@
+"use client"
+import React, {useRef, useState} from "react"
+import "./style.scss"
+
+const ThrottlingPage = () => {
+  const pRef = useRef<HTMLParagraphElement>(null)
+  const [delay, setDelay] = useState(1000)
+
+  function inputHandler(str: string) {
+    pRef.current!.textContent = str
+    console.log(1)
+  }
+
+  function handleDelay(delay: string) {
+    setDelay(parseInt(delay))
+  }
+
+  function throttle(fn: (args: string) => void, d = delay) {
+    let shouldWait = false
+    let waitingArgs: string | null = null
+
+    const f = (args: string): any => {
+      if (shouldWait) {
+        waitingArgs = args
+        return
+      }
+
+      fn(args)
+      shouldWait = true
+
+      setTimeout(() => {
+        shouldWait = false
+        if (typeof waitingArgs == "string") {
+          const wArgs = waitingArgs
+          waitingArgs = null
+          f(wArgs)
+        }
+      }, d)
+    }
+
+    return f
+  }
+
+  const throttledInputHandler = throttle(inputHandler)
+
+  return (
+    <div className="throttling-page">
+      <h1>Throttling</h1>
+      <input
+        type="number"
+        onInput={(e) => handleDelay((e.target as HTMLInputElement).value)}
+        placeholder="delay (default: 500ms)"
+      />
+      <input
+        type="text"
+        maxLength={5_000}
+        onInput={(e) =>
+          throttledInputHandler((e.target as HTMLInputElement).value)
+        }
+        placeholder="input"
+      />
+      <p className="label" data-label="example" ref={pRef}></p>
+    </div>
+  )
+}
+
+export default ThrottlingPage
--- src/components/project-list/projectList.tsx
+++ src/components/project-list/projectList.tsx
@@ -12,6 +12,11 @@ const ProjectList = () => {
           description="Debouncing in programming is a method to ensure that only one stable signal is recognized from a noisy input, like a button press, by adding a delay or filter to ignore rapid fluctuations."
           href="debouncing"
         />
+        <ProjectButton
+          title="Throttling"
+          description="Throttling in programming limits how often a function can be called within a specific timeframe. It's used to control the rate of execution, often to prevent performance issues or excessive resource usage."
+          href="throttling"
+        />
       </ul>
     </div>
   )
